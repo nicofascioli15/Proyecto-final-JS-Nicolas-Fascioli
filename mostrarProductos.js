a6cbabd0397df529e4990c7ce2ba89c65a87db54
nicolas fascioli <nicolas@fascioli.com.uy>
2023-04-28 14:23:15 -0300

    second commit -javascript

---
+++ mostrarProductos.js
@@ -0,0 +1,18 @@
+// creo el producto para mostrar en el html
+
+const mostrarProducto = (nuevoProducto) => {
+    const tarjetaProducto = document.createElement("div")
+    tarjetaProducto.className = "producto"
+    tarjetaProducto.innerHTML = `<h3>${nuevoProducto.titulo}</h3>
+                                <p>Id: ${nuevoProducto.id}</p>
+                                <p>${nuevoProducto.descripcion}</p>
+                                <p>$ ${nuevoProducto.precio}.-</p>
+                                <p>Stock: ${nuevoProducto.stock}</p>`
+
+    muestraProductos.append(tarjetaProducto)
+}
+
+const muestraProductos = document.querySelector("#muestraProductos")
+todosLosProductos.forEach((producto) => {
+    mostrarProducto(producto)
+});
--- script.js
+++ script.js
@@ -14,11 +14,11 @@ class producto {
     }
 }
 
-// cree el producto
+
 
 const agregarProductos = document.querySelector("#agregarProductos")
 
-agregarProductos.addEventListener("submit", (e)=>{
+    agregarProductos.addEventListener("submit", (e)=>{
     e.preventDefault()
     const datosProductos = e.target.children
     const nuevoProducto = new producto (datosProductos["titulo"].value, datosProductos["descripcion"].value, datosProductos["precio"].value, datosProductos["stock"].value)
@@ -27,25 +27,3 @@ agregarProductos.addEventListener("submit", (e)=>{
     agregarProductos.reset()
     mostrarProducto(nuevoProducto)
 })
-
-// voy a crear la tarjeta para mostrar por html el producto
-
-const mostrarProducto = (nuevoProducto) => {
-    const tarjetaProducto = document.createElement("div")
-    tarjetaProducto.className = "producto"
-    tarjetaProducto.innerHTML = `<h3>${nuevoProducto.titulo}</h3>
-                                <p>Id: ${nuevoProducto.id}</p>
-                                <p>${nuevoProducto.descripcion}</p>
-                                <p>$ ${nuevoProducto.precio}.-</p>
-                                <p>Stock: ${nuevoProducto.stock}</p>`
-
-    muestraProductos.append(tarjetaProducto)
-}
-
-const muestraProductos = document.querySelector("#muestraProductos")
-todosLosProductos.forEach((producto) => {
-    mostrarProducto(producto)
-});
-
-
-// voy a recorrer el array de los productos asi voy agregandole a cada uno la tarjeta que cree arriba
